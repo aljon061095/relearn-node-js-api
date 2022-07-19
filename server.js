0a1024c8b943fd9f49d3035e41b7d6c2efc4b61d
Aljon Quiambao <aljon.quiambao@mortgagechoice.com.au>
2022-07-19 14:52:59 +0800

    Updare registration API

--- server.js
+++ server.js
@@ -50,28 +50,13 @@ var dbConn = mysql.createConnection({
 dbConn.connect();
 
 // Retrieve all users 
-app.get('/users', function (req, res) {
-    dbConn.query('SELECT * FROM users', function (error, results, fields) {
+app.get('/kids', function (req, res) {
+    dbConn.query('SELECT * FROM kids', function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'users list.' });
+        return res.send({ error: false, data: results, message: 'kids list.' });
     });
 });
 
-// Retrieve user with id 
-app.get('/user/:id', function (req, res) {
-
-    let user_id = req.params.id;
-
-    if (!user_id) {
-        return res.status(400).send({ error: true, message: 'Please provide user_id' });
-    }
-
-    dbConn.query('SELECT * FROM users where id=?', user_id, function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'users list.' });
-    });
-
-});
 
 // Add a new user  
 app.post('/createuser', function (req, res) {
@@ -88,6 +73,23 @@ app.post('/createuser', function (req, res) {
     let grade = req.body.kids.grade;
     let kidsPassword = req.body.kids.kidsPassword;
 
+    //checking of duplicate email and nickname
+    dbConn.query('SELECT * FROM parent where email=?', email, function (error, results, fields) {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.status(409).send({ error: true, message: 'Your email is not available. Please provide a new one.' });
+            return;
+        } 
+    });
+
+    dbConn.query('SELECT * FROM kids where nickname=?', nickname, function (error, results, fields) {
+        if (error) throw error;
+        if (results.length > 0 && !res.headersSent) {
+            res.status(409).send({ error: true, message: 'Your kids nickname is not available. Please provide a new one.' });
+            return;
+        }
+    });
+
     //parent info
     dbConn.query("INSERT INTO parent SET ? ",
         {
@@ -100,6 +102,7 @@ app.post('/createuser', function (req, res) {
 
         let parentId = results.insertId;
         
+        //kids info
         if (error == null) {
             dbConn.query("INSERT INTO kids SET ? ",
             {
@@ -121,86 +124,9 @@ app.post('/createuser', function (req, res) {
     });
 });
 
-// Add a new user  
-app.post('/adduser', function (req, res) {
-
-    //let user = req.body.user;
-    let nickname = req.body.nickname;
-    let email = req.body.email;
-    let age = req.body.age;
-    let gender = req.body.gender;
-    let grade = req.body.grade;
-    let referral_code = req.body.referral_code;
-    let referral = Math.floor(10000000 + Math.random() * 90000000);
-    let password = req.body.password;
-
-    dbConn.query("INSERT INTO users SET ? ",
-        {
-            nickname: nickname,
-            email: email,
-            age: age,
-            gender: gender,
-            grade: grade,
-            referral_code: referral_code,
-            referral: referral,
-            password: password
-        }
-        , function (error, results, fields) {
-            if (error) throw error;
-
-        //    sendEmail(email, referral_code);
-        mail.sendEmail(email, referral);
-
-            return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
-        });
-});
-
-
-//  Update user with id
-app.put('/updateuser', function (req, res) {
-
-    let user_id = req.body.user_id;
-    let nickname = req.body.nickname;
-    let email = req.body.email;
-    let age = req.body.age;
-    let gender = req.body.gender;
-    let grade = req.body.grade;
-    let referral_code = req.body.referral_code;
-    let referral = req.body.referral;
-    let password = req.body.password;
-    // let user = req.body.user;
-
-    if (!user_id) {
-        return res.status(400).send({ error: user, message: 'Please provide user_id' });
-    }
-
-    dbConn.query("UPDATE users SET nickname = ?, email = ?, age = ?, gender = ?, grade = ?, referral_code = ?, referral = ?, password = ? WHERE id = ?", 
-            [nickname, email, age, gender, grade, referral_code, password, referral, user_id],
-             function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'user has been updated successfully.' });
-    });
-});
-
-
-//  Delete user
-app.delete('/deleteuser', function (req, res) {
-
-    let user_id = req.body.user_id;
-
-    if (!user_id) {
-        return res.status(400).send({ error: true, message: 'Please provide user_id' });
-    }
-    dbConn.query('DELETE FROM users WHERE id = ?', [user_id], function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'User has been deleted successfully.' });
-    });
-});
-
 // set port
 app.listen(3000, function () {
     console.log('Relearn Node app is running on port 3000');
 });
-
 
 module.exports = app;
